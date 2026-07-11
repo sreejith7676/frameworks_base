6ce1c46b5421b8e1548e109af314200e3c81c072
sreejith7676 <sreejithsss014@gmail.com>
2026-07-11 12:08:21 +0000

    Revert "services: Adding support for service injector"
    This reverts commit 36a93e37967dce951c472c5ca637c318ae5710aa.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -445,7 +445,6 @@ import com.android.internal.util.MemInfoReader;
 import com.android.internal.util.Preconditions;
 import com.android.internal.util.function.pooled.PooledLambda;
 import com.android.server.AlarmManagerInternal;
-import com.android.server.AxExtServiceFactory;
 import com.android.server.BootReceiver;
 import com.android.server.DeviceIdleInternal;
 import com.android.server.DisplayThread;
@@ -5366,10 +5365,6 @@ public class ActivityManagerService extends IActivityManager.Stub
             // Start PSI monitoring in LMKD if it was skipped earlier.
             ProcessList.startPsiMonitoringAfterBoot();
 
-            mHandler.postDelayed(() -> {
-                AxExtServiceFactory.onLateSystemReady();
-            }, 5000);
-
             mUserController.onBootComplete(
                     new IIntentReceiver.Stub() {
                         @Override
@@ -9255,8 +9250,6 @@ public class ActivityManagerService extends IActivityManager.Stub
             mComponentAliasResolver.onSystemReady(mConstants.mEnableComponentAlias,
                     mConstants.mComponentAliasOverrides);
             t.traceEnd(); // componentAlias
-            
-            AxExtServiceFactory.systemReady();
 
             t.traceEnd(); // PhaseActivityManagerReady
         }
