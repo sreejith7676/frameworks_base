7e28d35be5a67a922f5631f8b4418e8d38c62331
sreejith7676 <sreejithsss014@gmail.com>
2026-07-14 14:05:11 +0530

    Revert "base: Allow to hide screen capture status from apps [1/2]"
    This reverts commit 22c38ee7db4e700599f4123c22d5a18f946c297c.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10753,7 +10753,6 @@ public class WindowManagerService extends IWindowManager.Stub
             throw new SecurityException("Requires STATUS_BAR_SERVICE permission");
         }
         synchronized (mGlobalLock) {
-            if (shouldHideScreenCapture()) return new ArrayList<>();
             final DisplayContent displayContent = mRoot.getDisplayContent(displayId);
             if (displayContent == null) {
                 return new ArrayList<>();
@@ -10855,11 +10854,6 @@ public class WindowManagerService extends IWindowManager.Stub
     @EnforcePermission(android.Manifest.permission.DETECT_SCREEN_RECORDING)
     @Override
     public boolean registerScreenRecordingCallback(IScreenRecordingCallback callback) {
-
-      if (shouldHideScreenCapture()) {
-            return false;
-        }
-
         registerScreenRecordingCallback_enforcePermission();
         return mScreenRecordingCallbackController.register(callback);
     }
@@ -10872,11 +10866,6 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     void onProcessActivityVisibilityChanged(int uid, boolean visible) {
-
-     if (shouldHideScreenCapture()) {
-            return;
-        }
-
         mScreenRecordingCallbackController.onProcessActivityVisibilityChanged(uid, visible);
     }
 
@@ -11004,10 +10993,5 @@ public class WindowManagerService extends IWindowManager.Stub
                 mAtmService.updateFontScaleIfNeeded(userId);
             }
         }
-    }
-
-    private boolean shouldHideScreenCapture() {
-        return Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.HIDE_SCREEN_CAPTURE_STATUS, 0) != 0;
     }
 }
